d305e50f6f58d38eebdd2ce7eba3e23f59368d32
AkiraFukushima <h3.poteto@gmail.com>
2018-03-12 23:06:27 +0900

    refs #25 Add follow event card in notifications

--- src/renderer/components/TimelineSpace/Cards/Notification/Follow.vue
+++ src/renderer/components/TimelineSpace/Cards/Notification/Follow.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="follow">
-    follow
+  <div class="follow">
+    <div class="action">
+      <div class="action-mark">
+        <icon name="user-plus" scale="0.7"></icon>
+      </div>
+      <div class="action-detail">
+        <span class="bold">{{ message.account.display_name }}</span> is now following you
+      </div>
+      <div class="action-icon">
+        <img :src="message.account.avatar" />
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="fill-line"></div>
   </div>
 </template>
 
@@ -10,3 +22,47 @@ export default {
   props: ['message']
 }
 </script>
+
+<style lang="scss" scoped>
+.fill-line {
+  height: 1px;
+  background-color: #f2f6fc;
+  margin: 4px 0;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.follow {
+  margin-top: 8px;
+
+  .action {
+    margin-right: 8px;
+
+    .action-mark {
+      color: #409eff;
+      float: left;
+      width: 32px;
+      text-align: right;
+    }
+
+    .action-detail {
+      margin-left: 10px;
+      font-size: 14px;
+      float: left;
+    }
+
+    .action-icon {
+      width: 100%;
+      text-align: right;
+
+      img {
+        width: 16px;
+        height: 16px;
+        border-radius: 2px;
+      }
+    }
+  }
+}
+</style>
