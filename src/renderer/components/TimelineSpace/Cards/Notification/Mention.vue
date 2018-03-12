ffcf13f1a223bf18b21eadd19f4fb85af75412f1
AkiraFukushima <h3.poteto@gmail.com>
2018-03-12 23:13:55 +0900

    refs #25 Add mention event card in notifications

--- src/renderer/components/TimelineSpace/Cards/Notification/Mention.vue
+++ src/renderer/components/TimelineSpace/Cards/Notification/Mention.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="mention">
-    mention
+  <div class="mention">
+    <toot :message="message.status"></toot>
   </div>
 </template>
 
 <script>
+import Toot from '../Toot'
+
 export default {
   name: 'mention',
-  props: ['message']
+  props: ['message'],
+  components: { Toot }
 }
 </script>
