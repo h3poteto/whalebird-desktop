910ca8034935f8271dbc9549cc6aa2975fe8bda8
AkiraFukushima <h3.poteto@gmail.com>
2018-03-24 09:33:58 +0900

    refs #105 Get recent timeline in public

--- src/renderer/store/TimelineSpace/Public.js
+++ src/renderer/store/TimelineSpace/Public.js
@@ -1,4 +1,5 @@
 import { ipcRenderer } from 'electron'
+import Mastodon from 'mastodon-api'
 
 const Public = {
   namespaced: true,
@@ -8,6 +9,9 @@ const Public = {
   mutations: {
     appendTimeline (state, update) {
       state.timeline = [update].concat(state.timeline)
+    },
+    updateTimeline (state, messages) {
+      state.timeline = messages
     },
     updateToot (state, message) {
       state.timeline = state.timeline.map((toot) => {
@@ -27,12 +31,27 @@ const Public = {
     }
   },
   actions: {
-    startPublicStreaming ({ commit }, account) {
+    fetchPublicTimeline ({ state, commit, rootState }) {
+      return new Promise((resolve, reject) => {
+        const client = new Mastodon(
+          {
+            access_token: rootState.TimelineSpace.account.accessToken,
+            api_url: rootState.TimelineSpace.account.baseURL + '/api/v1'
+          }
+        )
+        client.get('/timelines/public', { limit: 40 }, (err, data, res) => {
+          if (err) return reject(err)
+          commit('updateTimeline', data)
+          resolve(res)
+        })
+      })
+    },
+    startPublicStreaming ({ state, commit, rootState }) {
       ipcRenderer.on('update-start-public-streaming', (event, update) => {
         commit('appendTimeline', update)
       })
       return new Promise((resolve, reject) => {
-        ipcRenderer.send('start-public-streaming', account)
+        ipcRenderer.send('start-public-streaming', rootState.TimelineSpace.account)
         ipcRenderer.once('error-start-public-streaming', (event, err) => {
           reject(err)
         })
