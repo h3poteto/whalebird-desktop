0dcca6316d02d82c4f08e30d0d8bb73857aac7f9
AkiraFukushima <h3.poteto@gmail.com>
2018-03-24 09:33:58 +0900

    refs #105 Get recent timeline in public

--- src/renderer/components/TimelineSpace/Public.vue
+++ src/renderer/components/TimelineSpace/Public.vue
@@ -15,17 +15,39 @@ export default {
   components: { Toot },
   computed: {
     ...mapState({
-      account: state => state.TimelineSpace.account,
       timeline: state => state.TimelineSpace.Public.timeline
     })
   },
   created () {
-    this.$store.dispatch('TimelineSpace/Public/startPublicStreaming', this.account)
+    const loading = this.$loading({
+      lock: true,
+      text: 'Loading',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)'
+    })
+    this.initialize()
+      .then(() => {
+        loading.close()
+      })
+      .catch(() => {
+        loading.close()
+      })
   },
   beforeDestroy () {
     this.$store.dispatch('TimelineSpace/Public/stopPublicStreaming')
   },
   methods: {
+    async initialize () {
+      try {
+        await this.$store.dispatch('TimelineSpace/Public/fetchPublicTimeline')
+      } catch (err) {
+        this.$message({
+          message: 'Could not fetch timeline',
+          type: 'error'
+        })
+      }
+      this.$store.dispatch('TimelineSpace/Public/startPublicStreaming')
+    },
     updateToot (message) {
       this.$store.commit('TimelineSpace/Public/updateToot', message)
     }
