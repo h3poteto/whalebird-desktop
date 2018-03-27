ef7bed8de28ded77f23e265c29e6e860b7db9cf5
AkiraFukushima <h3.poteto@gmail.com>
2018-03-27 15:33:20 +0900

    refs #146 Add lazy loading to home timeline

---
+++ src/renderer/store/TimelineSpace/Home.js
@@ -0,0 +1,35 @@
+import Mastodon from 'mastodon-api'
+
+const Home = {
+  namespaced: true,
+  state: {
+    lazyLoading: false
+  },
+  mutations: {
+    changeLazyLoading (state, value) {
+      state.lazyLoading = value
+    }
+  },
+  actions: {
+    lazyFetchTimeline ({ state, commit, rootState }, last) {
+      return new Promise((resolve, reject) => {
+        if (state.lazyLoading) {
+          return resolve()
+        }
+        commit('changeLazyLoading', true)
+        const client = new Mastodon(
+          {
+            access_token: rootState.TimelineSpace.account.accessToken,
+            api_url: rootState.TimelineSpace.account.baseURL + '/api/v1'
+          })
+        client.get('/timelines/home', { max_id: last.id, limit: 40 }, (err, data, res) => {
+          if (err) return reject(err)
+          commit('TimelineSpace/insertHomeTimeline', data, { root: true })
+          commit('changeLazyLoading', false)
+        })
+      })
+    }
+  }
+}
+
+export default Home
