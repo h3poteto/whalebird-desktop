dcfd87d0b773f4162b05d022e7ac0fb0783d6a9d
AkiraFukushima <h3.poteto@gmail.com>
2018-03-30 16:48:32 +0900

    refs #116 Show account profile in side bar

---
+++ src/renderer/components/TimelineSpace/Contents/SideBar/AccountProfile.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="account_profile">
+    <div class="header">
+      <div class="icon">
+        <img :src="account.avatar" />
+      </div>
+      <div class="username">
+        {{ username(account) }}
+      </div>
+      <div class="account">
+        @{{ account.acct }}
+      </div>
+      <div class="note" v-html="account.note"></div>
+    </div>
+    <el-row class="basic-info">
+      <el-col :span="8"class="info">
+        <div class="title">Posts</div>
+        <div class="count">{{ account.statuses_count }}</div>
+      </el-col>
+      <el-col :span="8"class="info">
+        <div class="title">Following</div>
+        <div class="count">{{ account.following_count }}</div>
+      </el-col>
+      <el-col :span="8"class="info">
+        <div class="title">Followers</div>
+        <div class="count">{{ account.followers_count }}</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'account-profile',
+  computed: {
+    ...mapState({
+      account: state => state.TimelineSpace.Contents.SideBar.AccountProfile.account
+    })
+  },
+  methods: {
+    username (account) {
+      if (account.display_name !== '') {
+        return account.display_name
+      } else {
+        return account.username
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background-color: rgba(255, 255, 255, 0.5);
+  text-align: center;
+  padding: 12px;
+  box-sizing: border-box;
+  word-wrap: break-word;
+  font-size: 14px;
+
+  .icon {
+    padding: 12px;
+
+    img {
+      width: 72px;
+      border-radius: 8px;
+    }
+  }
+
+  .username {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 24px;
+    margin: 0 auto 12px auto;
+  }
+
+  .account {
+    color: #409eff;
+  }
+}
+
+.basic-info {
+  border-top: solid 1px #dcdfe6;
+  border-bottom: solid 1px #dcdfe6;
+
+  .info {
+    border-left: solid 1px #dcdfe6;
+    padding: 8px 4px;
+
+    .title {
+      font-size: 10px;
+      color: #909399;
+    }
+
+    .count {
+      font-weight: 800;
+    }
+  }
+
+  .info:first-of-type {
+    border: none;
+  }
+}
+</style>
--- src/renderer/store/TimelineSpace/Contents/SideBar.js
+++ src/renderer/store/TimelineSpace/Contents/SideBar.js
@@ -1,17 +1,31 @@
+import AccountProfile from './SideBar/AccountProfile'
 
 const SideBar = {
   namespaced: true,
+  modules: {
+    AccountProfile
+  },
   state: {
-    openSideBar: false
+    openSideBar: false,
+    // 0: blank
+    // 1: account-profile
+    component: 0
   },
   mutations: {
     changeOpenSideBar (state, value) {
       state.openSideBar = value
+    },
+    changeComponent (state, value) {
+      state.component = value
     }
   },
   actions: {
     close ({ commit }) {
       commit('changeOpenSideBar', false)
+      commit('changeComponent', 0)
+    },
+    openAccountComponent ({ commit }) {
+      commit('changeComponent', 1)
     }
   }
 }
