9c9d14fb7b79da568dea0db856553e751cf6c7b3
AkiraFukushima <h3.poteto@gmail.com>
2018-04-01 17:48:39 +0900

    refs #132 List up accounts in preferences

--- src/renderer/components/Preferences/Account.vue
+++ src/renderer/components/Preferences/Account.vue
@@ -1,14 +1,65 @@
 <template>
 <div id="account">
   <h2>Account</h2>
+  <div class="connected-account">
+    <h3>Connected Accounts</h3>
+    <template>
+      <el-table
+        :data="accounts"
+        stripe
+        empty-text="No accounts"
+        style="width: 100%">
+        <el-table-column
+          prop="username"
+          label="Username"
+          width="240">
+        </el-table-column>
+        <el-table-column
+          prop="domain"
+          label="Domain">
+        </el-table-column>
+        <el-table-column
+          label="Association">
+          <template slot-scope="scope">
+            <el-button
+              @click.native.prevent="deleteAccount(scope.$index, accounts)"
+              type="text" icon="el-icon-close">
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+  </div>
 </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'account',
+  computed: {
+    ...mapState({
+      accounts: state => state.Preferences.Account.accounts
+    })
+  },
   created () {
     this.$store.commit('Preferences/changeActive', '2')
+    this.loadAccounts()
+  },
+  methods: {
+    loadAccounts () {
+      this.$store.dispatch('Preferences/Account/loadAccounts')
+        .catch(() => {
+          this.$message({
+            message: 'Failed to load accounts',
+            type: 'error'
+          })
+        })
+    },
+    deleteAccount (account) {
+      // TODO:
+    }
   }
 }
 </script>
--- src/renderer/store/Preferences/Account.js
+++ src/renderer/store/Preferences/Account.js
@@ -1,8 +1,31 @@
+import { ipcRenderer } from 'electron'
+
 const Account = {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    accounts: []
+  },
+  mutations: {
+    updateAccounts (state, accounts) {
+      state.accounts = accounts
+    }
+  },
+  actions: {
+    loadAccounts ({ commit }) {
+      return new Promise((resolve, reject) => {
+        ipcRenderer.send('list-accounts', 'list')
+        ipcRenderer.once('error-list-accounts', (event, err) => {
+          ipcRenderer.removeAllListeners('response-list-accounts')
+          reject(err)
+        })
+        ipcRenderer.once('response-list-accounts', (event, accounts) => {
+          ipcRenderer.removeAllListeners('error-list-accounts')
+          commit('updateAccounts', accounts)
+          resolve(accounts)
+        })
+      })
+    }
+  }
 }
 
 export default Account
