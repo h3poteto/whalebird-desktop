024d4a364b403e50d11701772aa8bffd7a1e0460
AkiraFukushima <h3.poteto@gmail.com>
2018-04-01 19:00:03 +0900

    refs #132 Show username in account preferences

--- src/renderer/store/Preferences/Account.js
+++ src/renderer/store/Preferences/Account.js
@@ -1,13 +1,32 @@
 import { ipcRenderer } from 'electron'
+import Mastodon from 'mastodon-api'
 
 const Account = {
   namespaced: true,
   state: {
-    accounts: []
+    accounts: [],
+    accountLoading: false
   },
   mutations: {
     updateAccounts (state, accounts) {
       state.accounts = accounts
+    },
+    mergeAccounts (state, accounts) {
+      // TODO: Save username in local db after authorize.
+      // This function can not support if user add multiple accounts which are exist in same domain.
+      // So when username is saved in local db, please compare with reference to username@domain.
+      state.accounts = state.accounts.map((a) => {
+        let account = a
+        accounts.map((acct) => {
+          if (acct.domain === a.domain) {
+            account = acct
+          }
+        })
+        return account
+      })
+    },
+    updateAccountLoading (state, value) {
+      state.accountLoading = value
     }
   },
   actions: {
@@ -24,6 +43,35 @@ const Account = {
           resolve(accounts)
         })
       })
+    },
+    fetchUsername ({ dispatch, commit }, accounts) {
+      return new Promise((resolve, reject) => {
+        dispatch('fetchAllAccounts', accounts)
+          .then((accounts) => {
+            commit('mergeAccounts', accounts)
+            resolve(accounts)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      })
+    },
+    fetchAllAccounts ({ commit }, accounts) {
+      return Promise.all(accounts.map((account) => {
+        return new Promise((resolve, reject) => {
+          const client = new Mastodon(
+            {
+              access_token: account.accessToken,
+              api_url: account.baseURL + '/api/v1'
+            })
+          client.get('/accounts/verify_credentials', (err, data, res) => {
+            if (err) return reject(err)
+            // The response doesn't have domain, so I cann't confirm that response and account is same.
+            // Therefore I merge account.
+            resolve(Object.assign(data, account))
+          })
+        })
+      }))
     }
   }
 }
