f39b8817226884b6e9ac8f72893a94fbd9d873d3
AkiraFukushima <h3.poteto@gmail.com>
2018-04-02 09:07:09 +0900

    refs #100 Swap forward a account order

--- src/renderer/store/Preferences/Account.js
+++ src/renderer/store/Preferences/Account.js
@@ -1,5 +1,4 @@
 import { ipcRenderer } from 'electron'
-import Mastodon from 'mastodon-api'
 
 const Account = {
   namespaced: true,
@@ -10,20 +9,6 @@ const Account = {
   mutations: {
     updateAccounts (state, accounts) {
       state.accounts = accounts
-    },
-    mergeAccounts (state, accounts) {
-      // TODO: Save username in local db after authorize.
-      // This function can not support if user add multiple accounts which are exist in same domain.
-      // So when username is saved in local db, please compare with reference to username@domain.
-      state.accounts = state.accounts.map((a) => {
-        let account = a
-        accounts.map((acct) => {
-          if (acct.domain === a.domain) {
-            account = acct
-          }
-        })
-        return account
-      })
     },
     updateAccountLoading (state, value) {
       state.accountLoading = value
@@ -44,35 +29,6 @@ const Account = {
         })
       })
     },
-    fetchUsername ({ dispatch, commit }, accounts) {
-      return new Promise((resolve, reject) => {
-        dispatch('fetchAllAccounts', accounts)
-          .then((accounts) => {
-            commit('mergeAccounts', accounts)
-            resolve(accounts)
-          })
-          .catch((err) => {
-            reject(err)
-          })
-      })
-    },
-    fetchAllAccounts ({ commit }, accounts) {
-      return Promise.all(accounts.map((account) => {
-        return new Promise((resolve, reject) => {
-          const client = new Mastodon(
-            {
-              access_token: account.accessToken,
-              api_url: account.baseURL + '/api/v1'
-            })
-          client.get('/accounts/verify_credentials', (err, data, res) => {
-            if (err) return reject(err)
-            // The response doesn't have domain, so I cann't confirm that response and account is same.
-            // Therefore I merge account.
-            resolve(Object.assign(data, account))
-          })
-        })
-      }))
-    },
     removeAccount ({ commit }, account) {
       return new Promise((resolve, reject) => {
         ipcRenderer.send('remove-account', account._id)
@@ -85,6 +41,22 @@ const Account = {
           resolve()
         })
       })
+    },
+    forwardAccount ({ commit }, account) {
+      return new Promise((resolve, reject) => {
+        ipcRenderer.send('forward-account', account)
+        ipcRenderer.once('error-forward-account', (event, err) => {
+          ipcRenderer.removeAllListeners('response-forward-account')
+          reject(err)
+        })
+        ipcRenderer.once('response-forward-account', (event) => {
+          ipcRenderer.removeAllListeners('error-forward-account')
+          resolve()
+        })
+      })
+    },
+    backwardAccount ({ commit }, account) {
+
     }
   }
 }
