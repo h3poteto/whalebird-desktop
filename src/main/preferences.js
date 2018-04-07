0a79606cf0dfb600ae530ba62412357ad8a6530b
AkiraFukushima <h3.poteto@gmail.com>
2018-04-07 22:40:57 +0900

    refs #193 Add sound setting in preferences, and save setting data in json

---
+++ src/main/preferences.js
@@ -0,0 +1,48 @@
+import empty from 'is-empty'
+import storage from 'electron-json-storage'
+
+const Base = {
+  general: {
+    sound: {
+      fav_rb: true,
+      toot: true
+    }
+  }
+}
+
+export default class Preferences {
+  constructor (path) {
+    this.path = path
+    this.data = Base
+  }
+
+  async load () {
+    try {
+      const preferences = await this.get()
+      if (empty(preferences)) return Base
+      return preferences
+    } catch (err) {
+      return Base
+    }
+  }
+
+  get () {
+    return new Promise((resolve, reject) => {
+      storage.get(this.path, (err, data) => {
+        if (err) return reject(err)
+        this.data = data
+        return resolve(data)
+      })
+    })
+  }
+
+  save (data) {
+    return new Promise((resolve, reject) => {
+      storage.set(this.path, data, (err) => {
+        if (err) return reject(err)
+        this.data = data
+        return resolve(data)
+      })
+    })
+  }
+}
