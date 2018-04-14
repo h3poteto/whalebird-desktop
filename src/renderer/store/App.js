dcf9a6452e5c9d43b1aa74efee59b564e78d02a7
AkiraFukushima <h3.poteto@gmail.com>
2018-04-14 16:24:56 +0900

    refs #98 Set color from theme color in preferences

--- src/renderer/store/App.js
+++ src/renderer/store/App.js
@@ -10,6 +10,8 @@ const App = {
       global_header_color: '#393f4f', // #4a5664
       side_menu_color: '#191b22', // #373d48
       primary_color: '#ffffff', // #303133
+      regular_color: '#ebeef5', // #606266
+      secondary_color: '#e4e7ed', // #909399
       border_color: '#606266', // #ebeef5
       header_menu_color: '#444b5d', // #ffffff
       wrapper_mask_color: 'rgba(0, 0, 0, 0.7)' // rgba(255, 255, 255, 0.7)
