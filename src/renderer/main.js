4d13881794dcaf58bad8e6d1714bebae610d5974
AkiraFukushima <h3.poteto@gmail.com>
2018-04-22 22:07:58 +0900

    fix: Block change route when same address

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,6 +6,8 @@ import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 import Popper from 'vue-popperjs'
 import 'vue-popperjs/dist/css/vue-popper.css'
+import { sync } from 'vuex-router-sync'
+import shortkey from 'vue-shortkey'
 
 import './assets/fonts/fonts.css'
 import App from './App'
@@ -13,8 +15,11 @@ import router from './router'
 import store from './store'
 
 Vue.use(ElementUI)
+Vue.use(shortkey)
 Vue.component('icon', Icon)
 Vue.component('popper', Popper)
+
+sync(store, router)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
