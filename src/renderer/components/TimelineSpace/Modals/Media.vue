fe9797c5615a78df1819eb0fc59f25180312f1ef
nasum <masaya.nasu@crowdworks.co.jp>
2018-05-18 17:38:16 +0900

    close #312 show movie on imageviewer

---
+++ src/renderer/components/TimelineSpace/Modals/Media.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="media">
+    <img :src="src" v-if="isImageFile()">
+    <video :src="src" v-else-if="isMovieFile()" controls></video>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String
+  },
+  methods: {
+    file_ext () {
+      return this.src.split('.').pop().toLowerCase()
+    },
+    isImageFile () {
+      return ['jpg', 'gif', 'png'].indexOf(this.file_ext()) >= 0
+    },
+    isMovieFile () {
+      return ['mp4'].indexOf(this.file_ext()) >= 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.media {
+  img, video {
+    max-width: 80%;
+    max-height: 80%;
+  }
+}
+</style>
+
+
