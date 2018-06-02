3610f9403f6493d310ce07ddd24fc23b42eb7db1
AkiraFukushima <h3.poteto@gmail.com>
2018-06-03 01:06:31 +0900

    refs #358 Open tag timeline page when click favourited

--- src/renderer/components/TimelineSpace/Contents/Cards/Toot.vue
+++ src/renderer/components/TimelineSpace/Contents/Cards/Toot.vue
@@ -100,6 +100,7 @@
 import moment from 'moment'
 import { shell, clipboard } from 'electron'
 import { mapState } from 'vuex'
+import { findLink, isTag } from '../../../utils/link'
 
 export default {
   name: 'toot',
@@ -297,32 +298,6 @@ export default {
       return !this.sensitive(message) || this.showAttachments
     }
   }
-}
-
-function findLink (target) {
-  if (target.localName === 'a') {
-    return target.href
-  }
-  if (target.parentNode === undefined || target.parentNode === null) {
-    return null
-  }
-  if (target.parentNode.getAttribute('class') === 'toot') {
-    return null
-  }
-  return findLink(target.parentNode)
-}
-
-function isTag (target) {
-  if (target.getAttribute('class') && target.getAttribute('class').includes('hashtag')) {
-    return true
-  }
-  if (target.parentNode === undefined || target.parentNode === null) {
-    return false
-  }
-  if (target.parentNode.getAttribute('class') === 'toot') {
-    return false
-  }
-  return isTag(target.parentNode)
 }
 </script>
 
