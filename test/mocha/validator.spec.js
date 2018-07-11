00933ce084b86a677f3f64132b56c0a3597854ad
AkiraFukushima <h3.poteto@gmail.com>
2018-07-12 00:46:55 +0900

    refs #434 Add mocha test and run in circleci

---
+++ test/mocha/validator.spec.js
@@ -0,0 +1,42 @@
+var assert = require('assert')
+import { domainFormat } from '../../src/renderer/utils/validator'
+
+describe('validator', () => {
+  describe('domainFormat', () => {
+    context('string contains protocol', () => {
+      const domain = 'https://mastodon.social'
+      it('should not match', () => {
+        const res = domain.search(domainFormat)
+        assert.equal(res, -1)
+      })
+    })
+    context('string contains account name', () => {
+      const domain = 'h3_poteto@mastodon.social'
+      it('should not match', () => {
+        const res = domain.search(domainFormat)
+        assert.equal(res, -1)
+      })
+    })
+    context('string is gTLD domain', () => {
+      const domain = 'mastodon.social'
+      it('should match', () => {
+        const res = domain.search(domainFormat)
+        assert.equal(res, 0)
+      })
+    })
+    context('string is subdomain', () => {
+      const domain = 'music.mastodon.social'
+      it('should match', () => {
+        const res = domain.search(domainFormat)
+        assert.equal(res, 0)
+      })
+    })
+    context('string is jp domain', () => {
+      const domain = 'mstdn.co.jp'
+      it('should match', () => {
+        const res = domain.search(domainFormat)
+        assert.equal(res, 0)
+      })
+    })
+  })
+})
