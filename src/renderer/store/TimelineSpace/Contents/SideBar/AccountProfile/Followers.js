25aa788c7ece685bf715ac1160f8938e0c933e03
AkiraFukushima <h3.poteto@gmail.com>
2018-10-15 23:22:14 +0900

    refs #657 Show relationships and follow/unfollow button in profile

--- src/renderer/store/TimelineSpace/Contents/SideBar/AccountProfile/Followers.js
+++ src/renderer/store/TimelineSpace/Contents/SideBar/AccountProfile/Followers.js
@@ -3,11 +3,15 @@ import Mastodon from 'megalodon'
 const Followers = {
   namespaced: true,
   state: {
-    followers: []
+    followers: [],
+    relationships: []
   },
   mutations: {
     updateFollowers (state, users) {
       state.followers = users
+    },
+    updateRelationships (state, relations) {
+      state.relationships = relations
     }
   },
   actions: {
@@ -19,8 +23,24 @@ const Followers = {
       )
       return client.get(`/accounts/${account.id}/followers`, { limit: 80 })
         .then(res => {
-          commit('TimelineSpace/Contents/SideBar/AccountProfile/changeLoading', false, { root: true })
           commit('updateFollowers', res.data)
+          return res.data
+        })
+        .finally(() => {
+          commit('TimelineSpace/Contents/SideBar/AccountProfile/changeLoading', false, { root: true })
+        })
+    },
+    fetchRelationships ({ commit, rootState }, accounts) {
+      const ids = accounts.map(a => a.id)
+      const client = new Mastodon(
+        rootState.TimelineSpace.account.accessToken,
+        rootState.TimelineSpace.account.baseURL + '/api/v1'
+      )
+      return client.get(`/accounts/relationships`, {
+        id: ids
+      })
+        .then(res => {
+          commit('updateRelationships', res.data)
           return res.data
         })
     }
