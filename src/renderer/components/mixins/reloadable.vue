8bcec75270bfa551f5935017ee9ca5df1e2d4a29
AkiraFukushima <h3.poteto@gmail.com>
2018-11-04 14:36:48 +0900

    refs #662 Reload direct messages in reloadable

--- src/renderer/components/mixins/reloadable.vue
+++ src/renderer/components/mixins/reloadable.vue
@@ -12,12 +12,15 @@ export default {
       })
       await this.$store.dispatch('TimelineSpace/stopUserStreaming')
       await this.$store.dispatch('TimelineSpace/stopLocalStreaming')
+      await this.$store.dispatch('TimelineSpace/stopDirectMessagesStreaming')
 
       await this.$store.dispatch('TimelineSpace/Contents/Home/fetchTimeline', account)
       await this.$store.dispatch('TimelineSpace/Contents/Local/fetchLocalTimeline', account)
+      await this.$store.dispatch('TimelineSpace/Contents/DirectMessages/fetchTimeline', account)
 
       this.$store.dispatch('TimelineSpace/startUserStreaming', account)
       this.$store.dispatch('TimelineSpace/startLocalStreaming', account)
+      this.$store.dispatch('TimelineSpace/startDirectMessagesStreaming', account)
       return account
     }
   }
