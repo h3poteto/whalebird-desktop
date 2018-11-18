a313b95625ebe0ec69c9d915c27cf7ab38917a70
L. E. Segovia <13498015+amyspark@users.noreply.github.com>
2018-11-18 12:05:57 +0000

    Enable a vue-loading overlay for the media viewer
    This commit modifies the CSS for the media viewer heavily. Please report
    back if there are any issues!

--- src/renderer/components/TimelineSpace/Modals/Media.vue
+++ src/renderer/components/TimelineSpace/Modals/Media.vue
@@ -1,10 +1,18 @@
 <template>
-  <video :src="src" v-if="isMovieFile()" controls></video>
-  <video :src="src" v-else-if="isGIF()" autoplay loop></video>
-  <img :src="src" v-else>
+  <div
+    id="current-media"
+    v-loading="loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)"
+  >
+    <video :src="src" v-if="isMovieFile()" controls v-on:loadstart="loaded()"></video>
+    <video :src="src" v-else-if="isGIF()" autoplay loop v-on:loadstart="loaded()"></video>
+    <img :src="src" v-else v-on:load="loaded()">
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   props: {
     src: {
@@ -16,22 +24,35 @@ export default {
       default: ''
     }
   },
+  computed: {
+    ...mapState({
+      loading: state => state.TimelineSpace.Modals.ImageViewer.loading
+    })
+  },
   methods: {
     isMovieFile () {
       return ['video'].includes(this.type)
     },
     isGIF () {
       return ['gifv'].includes(this.type)
+    },
+    async loaded () {
+      this.$store.dispatch('TimelineSpace/Modals/ImageViewer/loaded')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-img,
-video {
+#current-media {
   max-width: 80%;
-  max-height: 80%;
+  min-width: 10%;
+  height: 80%;
+
+  img,
+  video {
+    max-height: 100%;
+  }
 }
 </style>
 
