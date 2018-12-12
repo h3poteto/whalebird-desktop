c8b044376b931eacb0db64282565b3c6fe9ac00b
L. E. Segovia <13498015+amyspark@users.noreply.github.com>
2018-12-12 00:19:47 +0000

    Make Settings accessible

--- src/renderer/components/Settings/General.vue
+++ src/renderer/components/Settings/General.vue
@@ -1,20 +1,22 @@
 <template>
 <div id="general">
-  <div class="toot section">
+  <h2>{{ $t('settings.general.title') }}</h2>
+  <el-form class="toot section" label-width="250px" label-position="right" size="medium">
     <h3>{{ $t('settings.general.toot.title') }}</h3>
-    <p class="description">{{ $t('settings.general.toot.visibility.description') }}</p>
-    <el-select v-model="tootVisibility" placeholder="visibility">
-      <el-option
-        v-for="v in visibilities"
-        :key="v.value"
-        :label="$t(v.name)"
-        :value="v.value">
-      </el-option>
-    </el-select>
-    <p class="description">{{ $t('settings.general.toot.sensitive.description') }}</p>
-    <el-switch v-model="tootSensitive">
-    </el-switch>
-  </div>
+    <el-form-item for="visibility" :label="$t('settings.general.toot.visibility.description')">
+      <el-select id="visibility" v-model="tootVisibility" placeholder="visibility">
+        <el-option
+          v-for="v in visibilities"
+          :key="v.value"
+          :label="$t(v.name)"
+          :value="v.value">
+        </el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item for="sensitive" :label="$t('settings.general.toot.sensitive.description')">
+      <el-switch id="sensitive" v-model="tootSensitive"></el-switch>
+    </el-form-item>
+  </el-form>
 </div>
 </template>
 
@@ -58,12 +60,12 @@ export default {
 
 <style lang="scss" scoped>
 #general {
-  .description {
-    margin: 32px 0 20px;
-  }
-
-  .section {
+  .section /deep/ {
     margin-bottom: 40px;
+
+    .el-form-item__label {
+      color: var(--theme-primary-color);
+    }
   }
 }
 </style>
