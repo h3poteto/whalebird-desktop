02e0ba5b97e0457fffabbc612c8a9086bb31776b
AkiraFukushima <h3.poteto@gmail.com>
2018-12-26 20:29:05 +0900

    Use jest for unit tests instead of mocha

--- spec/unit/suggestText.spec.js
+++ spec/unit/suggestText.spec.js
@@ -2,7 +2,7 @@ import assert from 'assert'
 import suggestText from '../../src/renderer/utils/suggestText'
 
 describe('account', () => {
-  context('Only account name', () => {
+  describe('Only account name', () => {
     const str = '@h3pote'
     it('should match', () => {
       const [start, word] = suggestText(str, 7)
@@ -10,7 +10,7 @@ describe('account', () => {
       assert.strictEqual(start, 1)
     })
   })
-  context('Beginning of the sentence', () => {
+  describe('Beginning of the sentence', () => {
     const str = '@h3pote toot body'
     it('should match', () => {
       const [start, word] = suggestText(str, 7)
@@ -18,7 +18,7 @@ describe('account', () => {
       assert.strictEqual(start, 1)
     })
   })
-  context('Halfway of the sentence', () => {
+  describe('Halfway of the sentence', () => {
     const str = 'toot body @h3pote toot'
     it('should match', () => {
       const [start, word] = suggestText(str, 17)
@@ -26,7 +26,7 @@ describe('account', () => {
       assert.strictEqual(start, 11)
     })
   })
-  context('End of the sentence', () => {
+  describe('End of the sentence', () => {
     const str = 'toot body @h3pote'
     it('should match', () => {
       const [start, word] = suggestText(str, 17)
@@ -34,7 +34,7 @@ describe('account', () => {
       assert.strictEqual(start, 11)
     })
   })
-  context('No space', () => {
+  describe('No space', () => {
     const str = 'tootbody@h3pote'
     it('should not match', () => {
       const [start, word] = suggestText(str, 15)
