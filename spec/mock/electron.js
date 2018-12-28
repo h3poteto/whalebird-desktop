434bdc6a5f1fbaa56022a62d6a72df734aee9bcb
AkiraFukushima <h3.poteto@gmail.com>
2018-12-29 02:06:36 +0900

    refs #209 Fix Login spec to use ipc mock

--- spec/mock/electron.js
+++ spec/mock/electron.js
@@ -1,6 +1,9 @@
-export const ipcRenderer = {
-  send: jest.fn(),
-  on: jest.fn(),
-  once: jest.fn(),
-  removeAllListeners: jest.fn()
-}
+const { ipcRenderer, ipcMain } = require('electron-ipc-mock')()
+// export const ipcRenderer = {
+//   send: jest.fn(),
+//   on: jest.fn(),
+//   once: jest.fn(),
+//   removeAllListeners: jest.fn()
+// }
+module.exports.ipcRenderer = ipcRenderer
+module.exports.ipcMain = ipcMain
