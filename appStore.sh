f65c9241433a28147ccc434645cd43c5a897bf58
barrett <barrettbreshears@gmail.com>
2019-01-31 17:16:31 -0700

    update

--- appStore.sh
+++ appStore.sh
@@ -3,11 +3,11 @@ set -x
 # Name of your app.
 APP="Roma"
 # The path of your app to sign.
-APP_PATH="./packages/Roma-mas-x64/Roma.app"
+APP_PATH="./build/mac/Roma.app"
 # The path to the location you want to put the signed package.
 RESULT_PATH="./packages/$APP.pkg"
 # The name of certificates you requested.
-APP_KEY="3rd Party Mac Developer Application: VM DIGITAL LLC (5877VK992M)"
+APP_KEY="Developer ID Application: Sledge Dev Inc (9LH57GG343)"
 INSTALLER_KEY="3rd Party Mac Developer Installer: VM DIGITAL LLC (5877VK992M)"
 # The path of your plist files.
 CHILD_PLIST="./plist/child.plist"
@@ -36,4 +36,4 @@ codesign -s "$APP_KEY" -f --entitlements "$LOGINHELPER_PLIST" "$APP_PATH/Content
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$APP_PATH/Contents/MacOS/$APP"
 codesign -s "$APP_KEY" -f --entitlements "$PARENT_PLIST" "$APP_PATH"
 
-productbuild --component "$APP_PATH" /Applications --sign "$INSTALLER_KEY" "$RESULT_PATH"
+# productbuild --component "$APP_PATH" /Applications --sign "$INSTALLER_KEY" "$RESULT_PATH"
