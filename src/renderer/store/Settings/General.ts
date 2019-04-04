142671ea49e1a78b8a1bde1777f28eb6ebd0ec42
AkiraFukushima <h3.poteto@gmail.com>
2019-04-04 23:52:08 +0900

    refs #850 Replace Settings with typescript

---
+++ src/renderer/store/Settings/General.ts
@@ -0,0 +1,84 @@
+import Mastodon, { Account } from 'megalodon'
+import Visibilities from '~/src/constants/visibility'
+import { Module, MutationTree, ActionTree } from 'vuex'
+import Visibility from '~/src/types/visibility'
+
+export interface GeneralState {
+  visibility: number,
+  sensitive: boolean
+}
+
+const state = (): GeneralState => ({
+  visibility: Visibilities.Public.value,
+  sensitive: false
+})
+
+export const MUTATION_TYPES = {
+  CHANGE_VISIBILITY: 'changeVisibility',
+  CHANGE_SENSITIVE: 'changeSensitive'
+}
+
+const mutations: MutationTree<GeneralState> = {
+  [MUTATION_TYPES.CHANGE_VISIBILITY]: (state, value: number) => {
+    state.visibility = value
+  },
+  [MUTATION_TYPES.CHANGE_SENSITIVE]: (state, value: boolean) => {
+    state.sensitive = value
+  }
+}
+
+// TODO: use type of rootState
+const actions: ActionTree<GeneralState, any> = {
+  fetchSettings: async ({ commit, rootState }): Promise<Account> => {
+    const client = new Mastodon(
+      rootState.TimelineSpace.account.accessToken,
+      rootState.TimelineSpace.account.baseURL + '/api/v1'
+    )
+    const res = await client.get<Account>('/accounts/verify_credentials')
+    const visibility: Visibility | undefined = Object.values(Visibilities as Array<Visibility>).find((v) => {
+      return v.key === res.data.source!.privacy
+    })
+    commit(MUTATION_TYPES.CHANGE_VISIBILITY, visibility!.value)
+    commit(MUTATION_TYPES.CHANGE_SENSITIVE, res.data.source!.sensitive)
+    return res.data
+  },
+  setVisibility: async ({ commit, rootState }, value: number) => {
+    const client = new Mastodon(
+      rootState.TimelineSpace.account.accessToken,
+      rootState.TimelineSpace.account.baseURL + '/api/v1'
+    )
+    const visibility = Object.values(Visibilities as Array<Visibility>).find((v) => {
+      return v.value === value
+    })
+    const res = await client.patch<Account>('/accounts/update_credentials', {
+      source: {
+        privacy: visibility!.key
+      }
+    })
+    commit(MUTATION_TYPES.CHANGE_VISIBILITY, visibility!.value)
+    return res.data
+  },
+  setSensitive: async ({ commit, rootState }, value: boolean) => {
+    const client = new Mastodon(
+      rootState.TimelineSpace.account.accessToken,
+      rootState.TimelineSpace.account.baseURL + '/api/v1'
+    )
+    const res = await client.patch<Account>('/accounts/update_credentials', {
+      source: {
+        sensitive: value
+      }
+    })
+    commit(MUTATION_TYPES.CHANGE_SENSITIVE, value)
+    return res.data
+  }
+}
+
+// TODO: use type of rootState
+const General: Module<GeneralState, any> = {
+  namespaced: true,
+  state: state,
+  mutations: mutations,
+  actions: actions
+}
+
+export default General
---
+++ src/types/visibility.ts
@@ -0,0 +1,5 @@
+export default interface Visibility {
+  name: string,
+  value: number,
+  key: string
+}
