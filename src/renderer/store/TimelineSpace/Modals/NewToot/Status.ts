563b66c79327af1a442fae6397b01075c0632cbb
AkiraFukushima <h3.poteto@gmail.com>
2019-04-11 00:24:34 +0900

    [eslint] refs #850 Fix indent spaces for NewToot

--- src/renderer/store/TimelineSpace/Modals/NewToot/Status.ts
+++ src/renderer/store/TimelineSpace/Modals/NewToot/Status.ts
@@ -30,23 +30,19 @@ export const MUTATION_TYPES = {
 
 const mutations: MutationTree<StatusState> = {
   [MUTATION_TYPES.UPDATE_FILTERED_ACCOUNTS]: (state, accounts: Array<Account>) => {
-    state.filteredAccounts = accounts.map((a) => {
-      return {
-        name: `@${a.acct}`,
-        image: null
-      }
-    })
+    state.filteredAccounts = accounts.map(a => ({
+      name: `@${a.acct}`,
+      image: null
+    }))
   },
   [MUTATION_TYPES.CLEAR_FILTERED_ACCOUNTS]: (state) => {
     state.filteredAccounts = []
   },
   [MUTATION_TYPES.UPDATE_FILTERED_HASHTAGS]: (state, tags: Array<Tag>) => {
-    state.filteredHashtags = tags.map((t) => {
-      return {
-        name: `#${t}`,
-        image: null
-      }
-    })
+    state.filteredHashtags = tags.map(t => ({
+      name: `#${t}`,
+      image: null
+    }))
   },
   [MUTATION_TYPES.CLEAR_FILTERED_ACCOUNTS]: (state) => {
     state.filteredHashtags = []
@@ -70,7 +66,7 @@ const actions: ActionTree<StatusState, RootState> = {
       rootState.TimelineSpace.account.baseURL + '/api/v1'
     )
     const res: Response<Results> = await client.get<Results>('/search', { q: word })
-      commit(MUTATION_TYPES.UPDATE_FILTERED_HASHTAGS, res.data.hashtags)
+    commit(MUTATION_TYPES.UPDATE_FILTERED_HASHTAGS, res.data.hashtags)
     if (res.data.hashtags.length === 0) throw new Error('Empty')
     return res.data.hashtags
   }
