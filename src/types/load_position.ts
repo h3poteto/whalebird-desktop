48e1408cc96bfa808347bf056ddc58423a66971f
AkiraFukushima <h3.poteto@gmail.com>
2019-04-14 19:11:27 +0900

    refs #850 Replace Show in Lists with typescript

--- src/types/load_position.ts
+++ src/types/load_position.ts
@@ -1,6 +1,13 @@
 import { Status, Account } from 'megalodon'
 
 export interface LoadPosition {
-  status: Status,
+  status: Status
+}
+
+export interface LoadPositionWithAccount extends LoadPosition {
   account: Account
+}
+
+export interface LoadPositionWithList extends LoadPosition {
+  list_id: number
 }
