6c3fe850f05c2a927698e595fe9dac7cd926fdfe
AkiraFukushima <h3.poteto@gmail.com>
2019-04-14 15:48:55 +0900

    refs #850 Replace Local with typescript

--- src/renderer/store/TimelineSpace/Contents.ts
+++ src/renderer/store/TimelineSpace/Contents.ts
@@ -2,7 +2,7 @@ import SideBar, { SideBarModuleState } from './Contents/SideBar'
 import Home, { HomeState } from './Contents/Home'
 import Notifications from './Contents/Notifications'
 import Favourites from './Contents/Favourites'
-import Local from './Contents/Local'
+import Local, { LocalState } from './Contents/Local'
 import Public from './Contents/Public'
 import Search from './Contents/Search'
 import Lists from './Contents/Lists'
@@ -16,7 +16,8 @@ export interface ContentsState {}
 
 export interface ContentsModuleState extends ContentsState {
   SideBar: SideBarModuleState,
-  Home: HomeState
+  Home: HomeState,
+  Local: LocalState
 }
 
 const state = (): ContentsState => ({})
