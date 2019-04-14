853da1e45eaf4a6f1ec2c67424355fc65373c3ea
AkiraFukushima <h3.poteto@gmail.com>
2019-04-14 17:37:50 +0900

    refs #850 Replace Search with typescript

---
+++ src/renderer/store/TimelineSpace/Contents/Search.ts
@@ -0,0 +1,35 @@
+import Account from './Search/Account'
+import Tag from './Search/Tag'
+import Toots from './Search/Toots'
+import { Module, MutationTree } from 'vuex'
+import { RootState } from '@/store'
+
+export interface SearchState {
+  loading: boolean
+}
+
+export interface SearchModuleState extends SearchState {
+}
+
+const state = (): SearchState => ({
+  loading: false
+})
+
+export const MUTATION_TYPES = {
+  CHANGE_LOADING: 'changeLoading'
+}
+
+const mutations: MutationTree<SearchState> = {
+  [MUTATION_TYPES.CHANGE_LOADING]: (state, loading: boolean) => {
+    state.loading = loading
+  }
+}
+
+const Search: Module<SearchState, RootState> = {
+  namespaced: true,
+  modules: { Account, Tag, Toots },
+  state: state,
+  mutations: mutations
+}
+
+export default Search
