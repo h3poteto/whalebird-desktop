8af4806d231d4bd54c4d614dd172ea067f7fe1b8
AkiraFukushima <h3.poteto@gmail.com>
2019-04-16 23:50:53 +0900

    refs #850 Replace unreadNotification with typescript

--- src/main/unreadNotification.ts
+++ src/main/unreadNotification.ts
@@ -1,8 +1,11 @@
 import { isEmpty } from 'lodash'
 import Datastore from 'nedb'
+import { UnreadNotification as Config } from '~/src/types/unreadNotification'
 
 export default class UnreadNotification {
-  constructor (path) {
+  private db: Datastore
+
+  constructor (path: string) {
     this.db = new Datastore({
       filename: path,
       autoload: true
@@ -21,19 +24,19 @@ export default class UnreadNotification {
         // Add unique index.
         this.db.ensureIndex({ fieldName: 'accountID', unique: true, sparse: true }, (err) => {
           if (err) reject(err)
-          resolve(null)
+          resolve({})
         })
       })
     })
   }
 
-  insertOrUpdate (accountID, obj) {
+  insertOrUpdate (accountID: string, config: Config): Promise<number> {
     return new Promise((resolve, reject) => {
       this.db.update(
         {
           accountID: accountID
         },
-        obj,
+        config,
         {
           upsert: true
         },
@@ -44,9 +47,9 @@ export default class UnreadNotification {
     })
   }
 
-  findOne (obj) {
+  findOne (obj: any): Promise<Config> {
     return new Promise((resolve, reject) => {
-      this.db.findOne(obj, (err, doc) => {
+      this.db.findOne<Config>(obj, (err, doc) => {
         if (err) return reject(err)
         if (isEmpty(doc)) return reject(new EmptyRecordError('empty'))
         resolve(doc)
@@ -55,9 +58,4 @@ export default class UnreadNotification {
   }
 }
 
-class EmptyRecordError extends Error {
-  constructor (msg) {
-    super(msg)
-    this.name = 'EmptyRecordError'
-  }
-}
+class EmptyRecordError extends Error {}
--- src/types/unreadNotification.ts
+++ src/types/unreadNotification.ts
@@ -1,4 +1,5 @@
 export type UnreadNotification = {
+  accountID?: string,
   direct: boolean,
   local: boolean,
   public: boolean
