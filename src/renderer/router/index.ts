f46e7ee7274114ec2055c2d265707d60a5af29ae
AkiraFukushima <h3.poteto@gmail.com>
2019-04-17 21:05:47 +0900

    refs #850 Replace router with typescript

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/login',
@@ -156,3 +156,5 @@ export default new Router({
     }
   ]
 })
+
+export default router
