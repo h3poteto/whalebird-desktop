f203a0c1d141dd95cf96eae60ed5e8836750aa7b
AkiraFukushima <h3.poteto@gmail.com>
2019-05-18 22:28:53 +0900

    refs #872 Update favourited toot in all timelines

--- src/renderer/store/TimelineSpace/Contents/Notifications.ts
+++ src/renderer/store/TimelineSpace/Contents/Notifications.ts
@@ -4,10 +4,10 @@ import { Module, MutationTree, ActionTree } from 'vuex'
 import { RootState } from '@/store'
 
 export interface NotificationsState {
-  lazyLoading: boolean,
-  heading: boolean,
-  notifications: Array<Notification>,
-  unreadNotifications: Array<Notification>,
+  lazyLoading: boolean
+  heading: boolean
+  notifications: Array<Notification>
+  unreadNotifications: Array<Notification>
   filter: string
 }
 
@@ -49,7 +49,7 @@ const mutations: MutationTree<NotificationsState> = {
   [MUTATION_TYPES.UPDATE_NOTIFICATIONS]: (state, notifications: Array<Notification>) => {
     state.notifications = notifications
   },
-  [MUTATION_TYPES.MERGE_NOTIFICATIONS]: (state) => {
+  [MUTATION_TYPES.MERGE_NOTIFICATIONS]: state => {
     state.notifications = state.unreadNotifications.slice(0, 80).concat(state.notifications)
     state.unreadNotifications = []
   },
@@ -57,10 +57,10 @@ const mutations: MutationTree<NotificationsState> = {
     state.notifications = state.notifications.concat(notifications)
   },
   [MUTATION_TYPES.UPDATE_TOOT]: (state, message: Status) => {
-    state.notifications = state.notifications.map((notification) => {
+    state.notifications = state.notifications.map(notification => {
       // I want to update toot only mention.
       // Because Toot component don't use status information when other patterns.
-      if (notification.status !== null && notification.status.id === message.id) {
+      if (notification.type === 'mention' && notification.status && notification.status.id === message.id) {
         const status = {
           status: message
         }
@@ -70,10 +70,10 @@ const mutations: MutationTree<NotificationsState> = {
       }
     })
   },
-  [MUTATION_TYPES.CLEAR_NOTIFICATIONS]: (state) => {
+  [MUTATION_TYPES.CLEAR_NOTIFICATIONS]: state => {
     state.notifications = []
   },
-  [MUTATION_TYPES.ARCHIVE_NOTIFICATIONS]: (state) => {
+  [MUTATION_TYPES.ARCHIVE_NOTIFICATIONS]: state => {
     state.notifications = state.notifications.slice(0, 30)
   },
   [MUTATION_TYPES.CHANGE_FILTER]: (state, filter: string) => {
@@ -83,10 +83,7 @@ const mutations: MutationTree<NotificationsState> = {
 
 const actions: ActionTree<NotificationsState, RootState> = {
   fetchNotifications: async ({ commit, rootState }): Promise<Array<Notification>> => {
-    const client = new Mastodon(
-      rootState.TimelineSpace.account.accessToken!,
-      rootState.TimelineSpace.account.baseURL + '/api/v1'
-    )
+    const client = new Mastodon(rootState.TimelineSpace.account.accessToken!, rootState.TimelineSpace.account.baseURL + '/api/v1')
     const res: Response<Array<Notification>> = await client.get<Array<Notification>>('/notifications', { limit: 30 })
 
     commit(MUTATION_TYPES.UPDATE_NOTIFICATIONS, res.data)
@@ -97,11 +94,9 @@ const actions: ActionTree<NotificationsState, RootState> = {
       return Promise.resolve(null)
     }
     commit(MUTATION_TYPES.CHANGE_LAZY_LOADING, true)
-    const client = new Mastodon(
-      rootState.TimelineSpace.account.accessToken!,
-      rootState.TimelineSpace.account.baseURL + '/api/v1'
-    )
-    return client.get<Array<Notification>>('/notifications', { max_id: lastNotification.id, limit: 30 })
+    const client = new Mastodon(rootState.TimelineSpace.account.accessToken!, rootState.TimelineSpace.account.baseURL + '/api/v1')
+    return client
+      .get<Array<Notification>>('/notifications', { max_id: lastNotification.id, limit: 30 })
       .then(res => {
         commit(MUTATION_TYPES.INSERT_NOTIFICATIONS, res.data)
         return res.data
