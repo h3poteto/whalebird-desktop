fd544560cfc1d60cb246772edff023c41d1a0034
AkiraFukushima <h3.poteto@gmail.com>
2019-11-19 22:38:57 +0900

    refs #1106 Add some empty language translation

--- src/renderer/components/Preferences/Language.vue
+++ src/renderer/components/Preferences/Language.vue
@@ -1,20 +1,15 @@
 <template>
-<div id="language">
-  <h2>{{ $t('preferences.language.title') }}</h2>
-  <el-form class="display-language section" label-position="top" size="small">
-    <el-form-item for="language" :label="$t('preferences.language.language_description')">
-      <el-select id="language" v-model="displayLanguage" placeholder="style">
-        <el-option
-          v-for="lang in languages"
-          :key="lang.key"
-          :label="lang.name"
-          :value="lang.key">
-        </el-option>
-      </el-select>
-      <p class="notice">{{ $t('preferences.language.notice') }}</p>
-    </el-form-item>
-  </el-form>
-</div>
+  <div id="language">
+    <h2>{{ $t('preferences.language.title') }}</h2>
+    <el-form class="display-language section" label-position="top" size="small">
+      <el-form-item for="language" :label="$t('preferences.language.language_description')">
+        <el-select id="language" v-model="displayLanguage" placeholder="style">
+          <el-option v-for="lang in languages" :key="lang.key" :label="lang.name" :value="lang.key"> </el-option>
+        </el-select>
+        <p class="notice">{{ $t('preferences.language.notice') }}</p>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -22,52 +17,52 @@ import Language from '~/src/constants/language'
 
 export default {
   name: 'language',
-  data () {
+  data() {
     return {
       languages: [
+        Language.cs,
         Language.de,
         Language.en,
+        Language.es_es,
         Language.fr,
+        Language.it,
         Language.ja,
         Language.ko,
+        Language.no,
         Language.pl,
-        Language.it,
+        Language.pt_pt,
+        Language.ru,
+        Language.sv_se,
         Language.zh_cn
       ]
     }
   },
   computed: {
     displayLanguage: {
-      get () {
+      get() {
         return this.$store.state.Preferences.Language.language.language
       },
-      set (value) {
-        this.$store.dispatch('Preferences/Language/changeLanguage', value)
-          .then(() => {
-            this.confirm()
-          })
+      set(value) {
+        this.$store.dispatch('Preferences/Language/changeLanguage', value).then(() => {
+          this.confirm()
+        })
       }
     }
   },
-  created () {
+  created() {
     this.$store.dispatch('Preferences/Language/loadLanguage')
   },
   methods: {
-    confirm () {
-      this.$confirm(
-        this.$t('preferences.language.confirm.message'),
-        this.$t('preferences.language.confirm.title'),
-        {
-          confirmButtonText: this.$t('preferences.language.confirm.ok'),
-          cancelButtonText: this.$t('preferences.language.confirm.cancel'),
-          type: 'warning'
-        }
-      )
+    confirm() {
+      this.$confirm(this.$t('preferences.language.confirm.message'), this.$t('preferences.language.confirm.title'), {
+        confirmButtonText: this.$t('preferences.language.confirm.ok'),
+        cancelButtonText: this.$t('preferences.language.confirm.cancel'),
+        type: 'warning'
+      })
         .then(() => {
           this.$store.dispatch('Preferences/Language/relaunch')
         })
-        .cancel(() => {
-        })
+        .cancel(() => {})
     }
   }
 }
