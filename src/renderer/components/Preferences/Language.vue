4840bd4715cc7bc291334d00eadba8b90651360a
AkiraFukushima <h3.poteto@gmail.com>
2020-12-27 00:29:21 +0900

    Fix cancel action for confirm in element-ui

--- src/renderer/components/Preferences/Language.vue
+++ src/renderer/components/Preferences/Language.vue
@@ -65,7 +65,7 @@ export default {
         .then(() => {
           this.$store.dispatch('Preferences/Language/relaunch')
         })
-        .cancel(() => {})
+        .catch(() => {})
     }
   }
 }
