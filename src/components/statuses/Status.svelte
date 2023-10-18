081053916928f9ce583a1b876849ccdda12932cb
AkiraFukushima <h3.poteto@gmail.com>
2023-10-19 01:29:33 +0900

    Display reblogged header

--- src/components/statuses/Status.svelte
+++ src/components/statuses/Status.svelte
@@ -7,35 +7,48 @@
   import Attachments from './Attachments.svelte'
 
   export let status: Entity.Status
+  $: originalStatus = status.reblog && !status.quote ? status.reblog : status
 </script>
 
-<div class="flex border-b mr-2 py-1">
-  <div class="avatar m-2">
-    <Avatar src={status.account.avatar} rounded />
-  </div>
-  <div class="status relative">
-    <div class="header flex justify-between">
-      <div class="account-name flex">
-        <span class="text-gray-950 text-ellipsis break-all overflow-hidden">
-          {@html emojify(status.account.display_name, status.account.emojis)}</span
-        >
-        <span class="text-gray-600 text-ellipsis break-all overflow-hidden">@{status.account.acct}</span>
+<div class="border-b mr-2 py-1">
+  {#if status.reblog && !status.quote}
+    <div class="flex">
+      <div class="avatar grid justify-items-end pr-2">
+        <Avatar src={status.account.avatar} rounded size="xs" />
       </div>
-      <div class="date text-gray-600 text-right">
-        <time datetime={status.created_at}>
-          {dayjs(status.created_at).format('YYYY-MM-DD HH:mm:ss')}
-        </time>
+      <div class="status">
+        {status.account.username} boosted
       </div>
     </div>
-    <div class="body text-gray-950 break-all overflow-hidden">
-      {@html emojify(status.content, status.emojis)}
+  {/if}
+  <div class="flex">
+    <div class="avatar p-2">
+      <Avatar src={originalStatus.account.avatar} rounded />
     </div>
-    {#if status.card}
-      <Card card={status.card} />
-    {/if}
-    {#if status.media_attachments.length > 0}
-      <Attachments attachments={status.media_attachments} />
-    {/if}
+    <div class="status relative">
+      <div class="header flex justify-between">
+        <div class="account-name flex">
+          <span class="text-gray-950 text-ellipsis break-all overflow-hidden">
+            {@html emojify(originalStatus.account.display_name, originalStatus.account.emojis)}</span
+          >
+          <span class="text-gray-600 text-ellipsis break-all overflow-hidden">@{originalStatus.account.acct}</span>
+        </div>
+        <div class="date text-gray-600 text-right">
+          <time datetime={originalStatus.created_at}>
+            {dayjs(originalStatus.created_at).format('YYYY-MM-DD HH:mm:ss')}
+          </time>
+        </div>
+      </div>
+      <div class="body text-gray-950 break-all overflow-hidden">
+        {@html emojify(originalStatus.content, originalStatus.emojis)}
+      </div>
+      {#if originalStatus.card}
+        <Card card={originalStatus.card} />
+      {/if}
+      {#if originalStatus.media_attachments.length > 0}
+        <Attachments attachments={originalStatus.media_attachments} />
+      {/if}
+    </div>
   </div>
 </div>
 
